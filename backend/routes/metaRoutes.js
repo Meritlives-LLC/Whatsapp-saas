0dcf89bae554cd97029e27e6c155056261f0fa1b
Samuel Onime <onimesamuel7@gmail.com>
2026-05-02 10:29:48 +0100

    added oauth

--- backend/routes/metaRoutes.js
+++ backend/routes/metaRoutes.js
@@ -7,6 +7,6 @@ router.get('/oauth-url', metaOAuthController.getOAuthUrl);
 router.get('/token-status', metaOAuthController.getTokenStatus);
 router.post('/select-phone', metaOAuthController.selectPhone);
 router.delete('/disconnect', metaOAuthController.disconnect);
-router.get('/callback', metaOAuthController.handleCallback);
+router.get('/oauth-callback', metaOAuthController.oauthCallback);
 
 module.exports = router;
