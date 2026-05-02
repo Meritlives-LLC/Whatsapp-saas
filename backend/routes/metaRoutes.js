a84312ef582f609c3f8c61a4722d93e26aa10a37
Samuel Onime <onimesamuel7@gmail.com>
2026-05-02 10:35:21 +0100

    reroute

--- backend/routes/metaRoutes.js
+++ backend/routes/metaRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const metaOAuthController = require('../controllers/metaOAuthController');
 
 router.get('/oauth-url', metaOAuthController.getOAuthUrl);
-router.get('/token-status', metaOAuthController.getTokenStatus);
+router.get('/token-status', metaOAuthController.tokenStatus);
 router.post('/select-phone', metaOAuthController.selectPhone);
 router.delete('/disconnect', metaOAuthController.disconnect);
 router.get('/oauth-callback', metaOAuthController.oauthCallback);
